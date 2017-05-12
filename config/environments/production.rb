e2c06bae80e035fa46c765a7c24a7e070dbe39f8
Quan Minh Nguyen <minhquan.nguyen@citynow.vn>
2017-05-12 16:39:01 +0700

    Config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://minhquan-todo-app.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://minhquan-todo-app.herokuapp.com', 'http://minhquan-todo-app.herokuapp.com']
+
 end
